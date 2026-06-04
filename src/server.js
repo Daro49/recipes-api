85b1178e8cb48311f3f8a728bee0b5f63a5e4c4f
Matej Daransky <xdaranm00@stud.fit.vutbr.cz>
2026-06-04 17:51:47 +0200

    feat: skeleton for API

---
+++ src/server.js
@@ -0,0 +1,18 @@
+require('dotenv').config();
+
+const express = require('express');
+const cors = require('cors');
+
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+
+app.get('/', (req, res) => {
+    console.log('Loaded');
+    res.send('Hello there');
+})
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`API running on port: ${PORT}`));
